a2839a44a1abca2d185a93f2c1b88113d0ddbf8c
Filip Frysz <ffrysz@gmail.com>
2021-11-08 20:54:33 +0100

    Add randomChoice function and loop to fill people ar

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+
+const gender = ['male', 'female'];
+const maleNames = ['John', 'Tom', 'Freddy', 'Luke', 'Peter', 'Jonatan', 'Stephen'];
+const femaleNames = ['Amanda', 'Jessica', 'Natalie', 'Stephanie', 'Ruth', 'Ashley'];
+const lastNames = ['Wick', 'Johnson', 'Parker', 'Skywalker', 'Simpson', 'Andersen'];
+
+const randomChoice = (arr) => {
+  return arr[Math.floor(arr.length * Math.random())];
+}
+
+const people = [];
+
+for (i = 1; i <= 20; i++) {
+  const personGender = randomChoice(gender);
+  const personFirstName = personGender === 'male' ? randomChoice(maleNames) : randomChoice(femaleNames);
+
+  const person = {
+    gender: personGender,
+    firstName: personFirstName,
+    lastName: randomChoice(lastNames),
+    age: Math.floor(Math.random() * 100),
+  };
+
+  people.push(person);
+}
+
+const data = JSON.stringify(people);
+
+fs.writeFile('people.json', data, (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
